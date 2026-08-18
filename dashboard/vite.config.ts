14d5f017fa4150e8b27bdd1b1c380c0fce350c43
BulusuHanuSharan <hanusharan@BulusuHanuSharans-MacBook-Air-2.local>
2026-08-18 23:21:41 +0530

    Fix Vite base path

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -5,12 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: '/AeroGuard/',
   plugins: [react(), tailwindcss()],
-})import { base: '/AeroGuard/', } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
 })
-
